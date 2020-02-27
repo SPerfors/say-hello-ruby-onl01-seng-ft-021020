8f1e1d8698dedb6b19c9792176e85273a4195592
Steven Perfors <stevecanbuild@gmail.com>
2020-02-27 17:13:38 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,4 @@
 # Build your say_hello method here
-def say_hello(name)
-  if (name === undefined)
-    puts "Hello Ruby Programmer!"
-  else
-    puts "Hello #{name}!"
-  end
+def say_hello(name = Ruby Programmer)
+  puts "Hello #{name}!"
 end
